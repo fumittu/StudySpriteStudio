736a62ce4c993a4a47a2533ca9144f6939fa7883
fumittu <38680002+fumittu@users.noreply.github.com>
2018-11-04 01:28:39 +0900

    Revert "SpriteStudioのサンプルコード"
    This reverts commit 710be8505de99d29aab19a663b9075918bce4cdf.

--- Android1/Android1.Packaging/ARM/Debug/Package/bin/Android1.Packaging-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-D:\test\test\Android1\Android1.Packaging\ARM\Debug\Package\bin\Android1.Packaging-debug-unaligned.apk : \
-D:\test\test\Android1\Android1.Packaging\ARM\Debug\Package\bin\Android1.Packaging.ap_ \
-D:\test\test\Android1\Android1.Packaging\ARM\Debug\Package\libs\armeabi-v7a\gdbserver \
-D:\test\test\Android1\Android1.Packaging\ARM\Debug\Package\libs\armeabi-v7a\libAndroid1.so \
--- Android1/Android1.Packaging/ARM/Debug/Package/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\test\test\Android1\Android1.Packaging\ARM\Debug\Package\bin\AndroidManifest.xml : \
-D:\test\test\Android1\Android1.Packaging\ARM\Debug\Package\AndroidManifest.xml \
